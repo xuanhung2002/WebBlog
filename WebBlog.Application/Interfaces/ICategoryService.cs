57819cac5bb22463575e8da63dfac24416087a57
Evan.Nguyen <evan.nguyen@avepoint.com>
2025-06-25 15:10:34 +0700

    add category controller

---
+++ WebBlog.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,8 @@
+﻿namespace WebBlog.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<CategoryDto> GetById(Guid id);
+        Task<List<CategoryDto>> GetAll();
+    }
+}
